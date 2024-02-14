1389d3fad9976f112063ab8f0c9081200c921fdd
martin265 <martinsilungwe12@gmail.com>
2024-02-14 12:00:43 +0200

    progress

---
+++ variable_types.js
@@ -0,0 +1,23 @@
+// the file will deal with all the variable types 
+let profileFirstName = "martin"; 
+let profileLastName = "silungwe"
+
+// variables storing numbers and they are two numbers 
+// the first one is a whole number also described as an integer
+// the second one is a decimal point number also call floating point number
+// or just floats
+let profileAge = 25;
+let customerChange = 908.8374;
+
+let userAge = 25 > 90;
+console.log(userAge);
+
+let profileName = ["ronald", "maggie", "chifundo", "wongani", "martin", true];
+// ========= to access values in array we use indexes and they start counting at 
+// the index of zero
+console.log(profileName[5]);
+
+// ============== sample object declaration here ============ //
+let firstObject = {firstName: "martin", lastName: "silungwe", age: 23, Location: "malawi"};
+// accessing the values in the object we use keys for example
+console.log(firstObject.firstName);
