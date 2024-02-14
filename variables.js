3c496e2d3efdcedf4558c6fe6ec26a78582e8c5a
martin265 <martinsilungwe12@gmail.com>
2024-02-14 11:08:31 +0200

    learning progress

--- variables.js
+++ variables.js
@@ -1,2 +1,39 @@
 let profileName = "martin";
 console.log(profileName);
+
+myName = "martin";
+
+function myProfileName() {
+    console.log(myName);
+}
+
+myProfileName();
+
+var myName;
+
+// this is a multiline comment in javascript
+var profileAge = 30;
+
+console.log(profileAge);
+
+// changing the value of a variable
+currentName = "Martin";
+currentAge = 89;
+
+firstName = "martin";
+lastName = "silungwe";
+profileEmail = "martinsilungwe12@gmail.com";
+
+// function that displays all the variables 
+function showVariables() {
+    console.log(firstName);
+    console.log(lastName);
+    console.log(profileEmail);
+}
+
+// =========== calling the function here =========== //
+showVariables();
+// naming convention for variables.
+// a variable must and not start with a number or a any symbol like an underscrole _
+// a variable name can start with a small letter and the rest of the words being capital letters
+// variables names are case sensitive
