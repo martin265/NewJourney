03a5ba0643e878dbdf0156a918d88f6f2bd479d4
martin265 <martinsilungwe12@gmail.com>
2024-02-14 16:36:45 +0200

    progress

---
+++ numbers.js
@@ -0,0 +1,34 @@
+// working with numbers will involve dealing with integers, floats,
+// decimals and bigint
+
+let firstAge = 90;
+let profileName = 89.983;
+
+console.log(firstAge);
+console.log(profileName);
+
+console.log(typeof(firstAge));
+console.log(typeof(profileName));
+
+// ======= rounding the decimal value to 2 decimal places 
+let profileNumber = 7847.83892766728663;
+console.log(profileNumber.toFixed(9));
+
+let myNumber = "89484"; 
+myNumber += 900;
+console.log(myNumber);
+console.log(typeof(myNumber));
+
+// =========== working with math calculations here ============= //
+let firstNumer = 997;
+let secodnNumber = 89276373;
+let answer = firstNumer - secodnNumber;
+console.log(answer.toFixed(5));
+
+// ================= // ============= working with number still ============= //
+let num1 = 90;
+num1;
+console.log(--num1);
+
+
+
