e2f33cc0b8639641887cb0efd9f81b3b17ef5a45
martin265 <martinsilungwe12@gmail.com>
2024-02-16 09:26:25 +0200

    first code commit of the day

--- strings.js
+++ strings.js
@@ -1,1 +1,48 @@
 // working with strings here ==========/
+const string = "all revolutionary things will not be televised";
+console.log(string);
+
+let firstName = "martin";
+let lastName = "Silungwe";
+
+const fullName = firstName;
+console.log(fullName);
+
+// ========= single qoute, double qoute and backsticks ========== //
+const single = 'single qoute string';
+const double  = "double qoute string";
+const backstick = `backstick string will be like this`;
+
+console.log(single);
+console.log(double);
+console.log(backstick);
+
+
+// =========== joining two strings together also called concatenation ======= //
+let greetings = "martin silungwe";
+console.log(`hello there, ${greetings}`);
+
+const profileFirstName = "martin";
+const profileLastName = "silungwe";
+
+console.log(`${profileFirstName} ${profileLastName}`);
+
+
+const song = "God is good all the time";
+const score = 90;
+const playback = 87;
+
+const message = `i like the song called ${song} which has a score of ${score} and a play back of ${(score / playback)*100}%`;
+
+
+console.log(message);
+
+
+// ============== using leterals for multiline capabilities ============= //
+const messageWorld = `alo of things are happening in the world at the moment
+but with God being our protector everything is possible`;
+console.log(messageWorld);
+
+
+
+
