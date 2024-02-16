db5ac5843b167d2aa3427afbc3819663fd80aa00
martin265 <martinsilungwe12@gmail.com>
2024-02-16 10:26:58 +0200

    still in progress

--- manipulating_strings.js
+++ manipulating_strings.js
@@ -14,4 +14,26 @@ else {
     console.log(`invalid email ${profileEmail}`);
 }
 
+// checking is a name starts with a capital letter nd ends with a full stop
+function validateProfileNames() {
+    try {
+        // getting the inputs from the users
+        const firstName = "marti";
+        const lastName = "silungwe!";
+
+        if (firstName.length < 5 && lastName.startsWith("123456789")) {
+            console.log("enter a valid first name please");
+        } 
+        else if (lastName.length > 90 && lastName.endsWith("!")) {
+            console.log("check your last name please");
+        }
+        else {
+            console.log("valid firstname and lastname");
+        }
+    } catch (error) {
+        console.log(`something went wromg at ${error}`);
+    }
+}
+
+
 
