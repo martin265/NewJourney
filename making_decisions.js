aa480d2010bb2b9e7a3b5caa865be7380229155c
martin265 <martinsilungwe12@gmail.com>
2024-03-05 09:53:20 +0200

    working on the building blocks

---
+++ making_decisions.js
@@ -0,0 +1,13 @@
+// ========== working with the if statements in here
+let childShopping = false;
+let childAllowance;
+
+if (childShopping == true) {
+    childAllowance = 90;
+    console.log(childAllowance);
+}
+else {
+    childAllowance = 0;
+    console.log(childAllowance);
+}
+
