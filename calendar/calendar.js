dd78b0cc73a153c7984dcce689e8fb2cea17940b
martin265 <martinsilungwe12@gmail.com>
2024-03-05 12:40:02 +0200

    creating functions

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -1,7 +1,20 @@
 // ====== passing values into a select control
 const months = document.querySelector("select")
+const monthsValue = document.querySelector("h1");
+
+// ======= binding the event listener here
+months.addEventListener("change", addMonths);
+
+// function to loop through 
+function createDays() {
+    
+}
 
 function addMonths() {
     const selectedValue = months.value;
     
+    // ========== checking the values here //
+    if (selectedValue === "January") {
+        monthsValue.textContent = "hello world"
+    }
 }
