65be61f772283dbd5d92132f2f44b486cf6f2b56
martin265 <martinsilungwe12@gmail.com>
2024-03-05 11:42:22 +0200

    working with loops

---
+++ Loops/for_loop.js
@@ -0,0 +1,6 @@
+// looping through an array of items here
+const profileNames = ["ronald", "maggie", "chifundo", "wongani"];
+
+for (const profileName of profileNames){
+    console.log(profileName);
+}
