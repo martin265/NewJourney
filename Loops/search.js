289793bc1fc75ab6b739c5ad2d97b1e9512ee48b
martin265 <martinsilungwe12@gmail.com>
2024-03-07 09:23:35 +0200

    adding the event listeners

--- Loops/search.js
+++ Loops/search.js
@@ -3,4 +3,13 @@ const button = document.querySelector("button");
 const message = document.querySelector("message");
 const input = document.querySelector("input");
 
+// ======== the array for the names will be here ==== //
+const allNames = ["ronald", "maggie", "chifundo", "marting", "justine"];
 // binding and event listener to the input field here ======= //
+input.addEventListener("click", searchNames);
+
+// ========= creating the function to search for the names here ======= //
+function searchNames() {
+    // getting the value when the name is entered //
+    
+}
