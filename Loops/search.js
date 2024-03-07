df42e68853451a88685215e93f93b2cc925158bf
martin265 <martinsilungwe12@gmail.com>
2024-03-07 09:29:57 +0200

    working on the search functionality

--- Loops/search.js
+++ Loops/search.js
@@ -10,7 +10,18 @@ input.addEventListener("click", searchNames);
 
 // ========= creating the function to search for the names here ======= //
 function searchNames() {
+    input.focus();
     // getting the value when the name is entered //
     const responseValue = input.value.toLowerCase();
     // looping through each item in the array to check if it matchets with the entered value ====== //
+    for (let response = 0; response < allNames.length; response++) {
+        // ======= checking if the value matches here ========= //
+        if (responseValue === allNames[response]) {
+            message.textContent = `the searched value is ${allNames[response]}`;
+            break;
+        }
+        else {
+            message.textContent = `the searched name is not available`;
+        }
+    }
 }
