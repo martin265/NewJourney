e41864e19076edc15e015e27325964f8371a50da
martin265 <martinsilungwe12@gmail.com>
2024-03-07 09:54:45 +0200

    working with the while loop

--- Loops/search.js
+++ Loops/search.js
@@ -6,22 +6,24 @@ const input = document.querySelector("input");
 // ======== the array for the names will be here ==== //
 const allNames = ["ronald", "maggie", "chifundo", "marting", "justine"];
 // binding and event listener to the input field here ======= //
-input.addEventListener("click", searchNames);
+button.addEventListener("click", searchNames);
 
 // ========= creating the function to search for the names here ======= //
 function searchNames() {
-    input.focus();
+    
     // getting the value when the name is entered //
     const responseValue = input.value.toLowerCase();
+    input.focus();
     // looping through each item in the array to check if it matchets with the entered value ====== //
-    for (let response = 0; response < allNames.length; response++) {
-        // ======= checking if the value matches here ========= //
-        if (responseValue === allNames[response]) {
-            message.textContent = `the searched value is ${allNames[response]}`;
+    
+    for (let profileNames of allNames) {
+        if (responseValue === profileNames[0]) {
+            console.log(responseValue);
             break;
         }
         else {
-            message.textContent = `the searched name is not available`;
+            console.log("error");
         }
+        
     }
 }
