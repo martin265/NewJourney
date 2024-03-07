ccb3a58deab88e631711991c3bec11f2f60d3a85
martin265 <martinsilungwe12@gmail.com>
2024-03-07 09:20:40 +0200

    binding event listener

--- Loops/search.js
+++ Loops/search.js
@@ -1,1 +1,6 @@
 // ======== working on the search function here
+const button = document.querySelector("button");
+const message = document.querySelector("message");
+const input = document.querySelector("input");
+
+// binding and event listener to the input field here ======= //
