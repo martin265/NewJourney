3c161fa01ad76c2d82512e2984b966771d4515d1
martin265 <martinsilungwe12@gmail.com>
2024-04-04 11:59:30 +0200

    working on the decimal places

--- Basic/basicMath.js
+++ Basic/basicMath.js
@@ -1,1 +1,10 @@
 // this will include all the math functions
+
+let profileAge = 90;
+let profileMoney = 90.3773;
+
+console.log(typeof(profileAge));
+
+// coverting floating point numbers to two decimal places 
+
+
