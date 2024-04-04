1f4ed093027b44e892d1ee01b23deb1e41555812
martin265 <martinsilungwe12@gmail.com>
2024-04-04 12:00:51 +0200

    rounding numbers

--- Basic/basicMath.js
+++ Basic/basicMath.js
@@ -7,4 +7,7 @@ console.log(typeof(profileAge));
 
 // coverting floating point numbers to two decimal places 
 
+let lostofDecimals = 90.836648673826773;
 
+let twodecimals = lostofDecimals.toFixed(3);
+console.log(twodecimals);
