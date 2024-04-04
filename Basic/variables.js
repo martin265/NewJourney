45af6f34e31cd932a16980497c6476d50ce9d83e
martin265 <martinsilungwe12@gmail.com>
2024-04-04 10:54:42 +0200

    working on variables

--- Basic/variables.js
+++ Basic/variables.js
@@ -4,3 +4,28 @@ console.log(profileName);
 
 let profileGender = "male";
 console.log(profileGender);
+
+
+let profileNames = "maggie";
+console.log(profileNames);
+
+// reassigning the variable names here ========= //
+let fullName = "mart silungwe";
+fullName = "maggie gama";
+console.log(fullName);
+
+
+// ============= storing diffrent types of variables ================ //
+let myAge = 90;
+console.log(myAge);
+
+
+let mySister = "chifundo and maggie";
+console.log(mySister);
+
+
+// =========== working with the arrays =========== //
+let familyProfile = ["ronald", "maggie", "chifundo", "wongani"];
+// accessing the single value in the array because the top is an array of names ======= //
+let extractedName = familyProfile[0];
+console.log(extractedName);
