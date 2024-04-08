51a806079d72ab916c923fe2839099860a616240
martin265 <martinsilungwe12@gmail.com>
2024-04-08 03:06:27 +0200

    using switch statements

--- if_statements.js
+++ if_statements.js
@@ -3,13 +3,13 @@ const para = document.querySelector("h1");
 const page = document.querySelector("body");
 let temperature = 80;
 // binding the event listener here ======//
-select.addEventListener("change", selectChange);
+select.addEventListener("change", checkStatus);
 
 
 function selectChange() {
     const value = select.value;
 
-    if (value === "sunny" && temperature == 90) {
+    if (value === "sunny" || temperature == 90) {
         para.textContent = "hello world"
     }
     else if (value === "snow") {
@@ -21,3 +21,22 @@ function selectChange() {
     }
 
 }
+
+// using switch statements from the same example
+function checkStatus() {
+    // getting the value to evaluate using the switch statements here ====== //
+    const choice = select.value;
+    switch (choice) {
+        case "sunny":
+            page.textContent = "the selected value is sunny";
+            break;
+        
+        case "cloudy":
+            page.textContent = "the selected value is cloudy";
+            break;
+        
+        default:
+            page.textContent = "nothing to see here";
+    }
+}
+
