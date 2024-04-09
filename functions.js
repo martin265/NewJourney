6ff0e650d2b7e98e9f219c81b0ad6eeb979a787d
martin265 <martinsilungwe12@gmail.com>
2024-04-09 15:50:39 +0200

    working with custome functions

--- functions.js
+++ functions.js
@@ -14,18 +14,12 @@ const input = document.querySelector("input");
 const content = document.querySelector("h1");
 const btn = document.querySelector("button");
 
-input.addEventListener("keyup", function keyPress() {
-    const choice = input.value;
-    content.textContent = `you have pressed ${choice}`;
-});
+btn.addEventListener("click", showMessage);
 
-// adding the event listener to the button here =====//
-btn.addEventListener("click", saveDetails);
-const customerMessage = [];
-function saveDetails() {
-    customerMessage.push(content);
-    console.log(`${customerMessage}`);
+function showMessage() {
+    
 }
+
 
 
 
