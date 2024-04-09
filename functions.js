a58ed3693dc3c57970fd92f3e0e38aea8fccbb27
martin265 <martinsilungwe12@gmail.com>
2024-04-09 16:10:53 +0200

    last commit for javascript today

--- functions.js
+++ functions.js
@@ -23,6 +23,20 @@ body.appendChild(panel);
 // ============= appending a message content to the panel here ========= //
 const message = document.createElement("p");
 message.textContent = "this is the alert dialog page";
+panel.appendChild(message);
+
+
+// adding a button to the panel for closing the alert dialog
+const btn = document.createElement("button");
+btn.textContent = "close window";
+panel.appendChild(btn);
+
+
+// ========= adding an event listener to the button here ========= //
+btn.addEventListener("click", function() {
+    panel.parentNode.removeChild(panel);
+});
+
 
 
 
