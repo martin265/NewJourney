c74786375b26a99b29fcb5ade48603ae12042521
martin265 <martinsilungwe12@gmail.com>
2024-04-09 16:02:44 +0200

    custom functions

--- functions.js
+++ functions.js
@@ -10,16 +10,19 @@ function profileName(name = "christ".toLocaleUpperCase()) {
 }
 
 
-const input = document.querySelector("input");
-const content = document.querySelector("h1");
-const btn = document.querySelector("button");
+// getting and setting values on the page here
+const body = document.body;
+const panel = document.createElement("div");
+panel.setAttribute("class", "msg");
 
-btn.addEventListener("click", showMessage);
 
-function showMessage() {
-    
-}
+// appending the div to the body here =========== //
+body.appendChild(panel);
 
+
+// ============= appending a message content to the panel here ========= //
+const message = document.createElement("p");
+message.textContent = "this is the alert dialog page";
 
 
 
