b0423d9e7f0792a614cde10ea7447a0f89499641
martin265 <martinsilungwe12@gmail.com>
2024-04-09 09:48:44 +0200

    using the map function

--- looping_code.js
+++ looping_code.js
@@ -1,1 +1,21 @@
 // the section for loops for (condition) another for loop
+// using the for of loop with the arrays
+
+const profileNames = ["martin", "ronald", "chifundo", "maggie", "justine"];
+
+
+for (const name of profileNames) {
+    console.log(name);
+}
+
+function chengeUpper(string) {
+    return string.toUpperCase();
+}
+
+const pets = ["lion", "leopad"];
+
+const new_array = pets.map(chengeUpper);
+
+console.log(new_array);
+
+// 
