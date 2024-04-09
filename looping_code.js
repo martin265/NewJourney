7e0a231616e5c526db1c405183258e08061f6318
martin265 <martinsilungwe12@gmail.com>
2024-04-09 09:52:36 +0200

    using the filter function

--- looping_code.js
+++ looping_code.js
@@ -18,4 +18,14 @@ const new_array = pets.map(chengeUpper);
 
 console.log(new_array);
 
-// 
+// using the map function to multply the numbers
+function multplyNumbers(number) {
+    return number % 2;
+}
+
+const profileNumbers = [289, 8, 67, 3, 4, 29];
+
+// pass the array to a new map function
+
+const newArray = profileNumbers.map(multplyNumbers);
+console.log(newArray);
