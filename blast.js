23c4bf44ef38dfe2cbdcc61cd05d7d84b420295d
martin265 <martinsilungwe12@gmail.com>
2024-04-09 14:53:06 +0200

    working with functions

--- blast.js
+++ blast.js
@@ -1,13 +1,1 @@
 // getting the values from the html page
-
-for (let counter = 0; counter <= 10; counter++) {
-    const newValue = Number(counter);
-    if (newValue == 4) {
-        console.log(newValue);
-        break;
-    }
-}
-
-
-
-
