ee3f75fdf340956491228915185c46ecc4c3648b
martin265 <martinsilungwe12@gmail.com>
2024-04-09 15:00:03 +0200

    working with functions

---
+++ functions.js
@@ -0,0 +1,10 @@
+function greetUser() {
+    const message = "hello world";
+    console.log(message.replace("hello", "martin"));
+}
+
+// calling or invoking the function here
+// functions with default parameters
+function profileName(name = "christ".toLocaleUpperCase()) {
+    
+}
