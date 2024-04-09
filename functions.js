cb95387c8df558a5a194a2ea688e6336a517e801
martin265 <martinsilungwe12@gmail.com>
2024-04-09 15:18:19 +0200

    working with textfield functions

--- functions.js
+++ functions.js
@@ -6,5 +6,17 @@ function greetUser() {
 // calling or invoking the function here
 // functions with default parameters
 function profileName(name = "christ".toLocaleUpperCase()) {
-    
+    console.log(` hello ${name}`);
 }
+
+
+const input = document.querySelector("input");
+const content = document.querySelector("h1");
+
+input.addEventListener("keyup", function keyPress() {
+    const choice = input.value;
+    content.textContent = `you have pressed ${choice}`;
+});
+
+
+
