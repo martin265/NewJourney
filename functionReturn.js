a962cdb72cfda15a54ac5f6cbb66c4c0b9bc0dd8
martin265 <martinsilungwe12@gmail.com>
2024-04-10 09:19:48 +0200

    function to draw a canvas

--- functionReturn.js
+++ functionReturn.js
@@ -1,1 +1,16 @@
 // getting the values using the dom manipulation
+const para = document.querySelector("p");
+
+
+
+
+function chooseName() {
+    const names = ["martin", "ronald", "chifundo"];
+    for (let name of names){
+        const content = name;
+        para.textContent = `${name}`;
+    }
+    
+}
+
+// function to draw a canvas on the screen
