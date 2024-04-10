5eca1a17fdc42df1bad74711dec55f325b0f8876
martin265 <martinsilungwe12@gmail.com>
2024-04-10 10:54:27 +0200

    validating the from fields

--- calculator.js
+++ calculator.js
@@ -1,5 +1,11 @@
 // getting the controls from the page here ========//
-const input = document.getElementsByName("results_number");
+const input = document.querySelector(`input[name="results_number"]`)
 const btn = document.querySelector("button");
 const results = document.querySelector("h1");
+const secondNumber = document.querySelector(`input[name="second_number"]`);
 
+// binding the event listener here ======
+btn.addEventListener("click", ()=>{
+    const firstNumber = input.value;
+    const secondInput = secondNumber.value
+});
