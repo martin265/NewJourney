a9c1d05be8d056c52faf1796273758d401fbaf29
martin265 <martinsilungwe12@gmail.com>
2024-04-10 10:44:00 +0200

    getting controls from the html page

--- calculator.js
+++ calculator.js
@@ -1,1 +1,5 @@
 // getting the controls from the page here ========//
+const input = document.getElementsByName("results_number");
+const btn = document.querySelector("button");
+const results = document.querySelector("h1");
+
