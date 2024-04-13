0607d789b644cb217ef2c3fc8ebae8e9eaaf52a9
martin265 <martinsilungwe12@gmail.com>
2024-04-13 09:23:57 +0200

    working on the events

--- calculator.js
+++ calculator.js
@@ -6,6 +6,11 @@ const secondNumber = document.querySelector(`input[name="second_number"]`);
 
 // binding the event listener here ======
 btn.addEventListener("click", ()=>{
-    const firstNumber = input.value;
-    const secondInput = secondNumber.value
+    const firstNumber = Number(input.value);
+    const secondInput = Number(secondNumber.value);
+    // checking if the values provide are numbers
+    const sumCalc = firstNumber + secondInput;
+    if (sumCalc) {
+        results.textContent = `${sumCalc}`;
+    }
 });
---
+++ events/event.js
@@ -0,0 +1,8 @@
+// getting the input from the form here
+const form = document.querySelector("form");
+const firstName = document.getElementById("first_name");
+const lastName = document.getElementById("last_name");
+const age = document.getElementById("age");
+const email = document.getElementById("email");
+
+// binding the event listener here
