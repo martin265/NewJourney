c9789fcc357cfc3ac41c68bf7f6964b088c6e97f
martin265 <martinsilungwe12@gmail.com>
2024-04-13 09:33:28 +0200

    validating the fields

--- events/event.js
+++ events/event.js
@@ -5,11 +5,19 @@ const lastName = document.getElementById("last_name");
 const age = document.getElementById("age");
 const email = document.getElementById("email");
 
+
+// getting the labels for the form here
+const firstNameLabel = document.getElementById("firstnameLabel");
+const errorMessage = document.getElementById("error_message");
+
 // binding the event listener here
 form.addEventListener("submit", (e)=>{
     // checking if the fields are empty here
     if (firstName.value === "" || lastName.value === "") {
         e.preventDefault();
-        console.log("please in the form");
+        firstName.style.border = "solid 2px red";
+        firstNameLabel.style.color = "red";
+        errorMessage.textContent = "fill in the blanks please";
+        errorMessage.style.color = "red"
     }
 });
