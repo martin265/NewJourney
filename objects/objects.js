d7bc4f5379c5ea6c82daaf52a0c4c9d283e07e56
martin265 <martinsilungwe12@gmail.com>
2024-04-13 10:25:53 +0200

    working with objects

--- objects/objects.js
+++ objects/objects.js
@@ -3,6 +3,12 @@ const person = {
     age: 25,
     bio: function() {
         console.log(`the users name ${this.name[0]} last name is ${this.name[1]} and the age is ${this.age}`);
+    },
+    introduction: function() {
+        console.log(`hello there ${this.name[0]}`);
     }
 };
+
+
+// using the object into another object here
 
