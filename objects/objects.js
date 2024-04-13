b00f8f6fbac9180eac9520f01775c844a3efe030
martin265 <martinsilungwe12@gmail.com>
2024-04-13 10:11:41 +0200

    creating objects

--- objects/objects.js
+++ objects/objects.js
@@ -1,1 +1,8 @@
-const person = {};
+const person = {
+    name: ["martin", "silungwe"],
+    age: 25,
+    bio: function() {
+        console.log(`the users name ${this.name[0]} last name is ${this.name[1]} and the age is ${this.age}`);
+    }
+};
+
